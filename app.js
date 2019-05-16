c4ba348c75b02a4401bad8fe08bdaa989db86dc6
anugrahdeovani <anugrahdeovani@gmail.com>
2019-05-17 02:08:10 +0700

    Make it better.

--- app.js
+++ app.js
@@ -1,98 +1,15 @@
-const http = require('http');
-const express = require ('express');
+const express = require("express");
 const app = express();
-const mysql = require('mysql');
-const cors = require ('cors')
-//Database Connection
-
-
-var con = mysql.createConnection(
-{
-    host : 'localhost',
-    user : 'root',
-    password : '',
-    database : 'Training-Amber' 
-})
-
-var db = mysql.createPool({
-  connectionLimit: 100,
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'Basic' 
-})
-
-var execute = (db, sql, args) => {
-  return new Promise((resolve, reject) => {
-    db.query(sql, args, (error, results, fields) => {
-      if (error) reject(error)
-      else resolve(results)
-    })
-  })
-}
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
 
 //using cors
-app.use(cors())
-
+app.use(cors());
+// set up BodyParser Middleware
+app.use(bodyParser.json({ limit: "100mb" })); // limit post data: 50 MB
+app.use(bodyParser.urlencoded({ extended: true }));
+// routes
+app.use("/", routes);
 //Setting Server
-app.listen(3000,()=>console.log('Server is running at 3000'));
-
-//test server/test
-app.get('/',(req,res)=>{res.send('NodeJS find location BackEnd by Deovani Anugrah')})
-
-// When user access database Person
-app.get('/location-bylonglat',(req,res)=> {
-    const params = req;
-    console.log(params);
-    
-    con.query("Select * FROM person", ( err, result) => {
-        let data=[];
-        result.forEach((item) => {
-            // console.log(item)
-            console.log(typeof(item.orangid))
-            data.push(
-                {
-                    type:"orangs",
-                    id:item.orang_id,
-                    attributes:
-                        {
-                            orangname: item.orang_name
-                        }
-                }   
-            )   
-        })
-        console.log(data);    
-        res.json(data)
-    })
-
-    //for Frans
-    let data = req.body.attributes
-     ,  longitude = data.longitude
-     ,  latitude = data.latitude
-
-    let sql = `SELECT desa, kecamatan, kabupaten, provinsi, region, area, branch, subbranch, cluster
-               FROM BTS_DESA WHERE langitude = ? AND latitude = ?`
-
-    execute(db, sql, [longitude, latitude]).then(response => {
-      let output = [];
-      response.forEach((item) => {
-        output.push({
-          desa : item.desa,
-          kecamatan : item.desa,
-          kabupaten : item.desa,
-          provinsi : item.desa,
-          region : item.desa,
-          area : item.desa,
-          branch : item.desa,
-          subbranch : item.desa,
-          cluster : item.desa
-        })
-      })
-      res.json(response);
-    }).catch(reason => {
-      res.json([{
-        reason
-      }])
-    })
-    
-})
+app.listen(3000, () => console.log("Server is running at 3000"));
---
+++ routes.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router();
+const mysql = require("mysql");
+
+//Database Connection
+var db = mysql.createPool({
+  connectionLimit: 100,
+  host: "localhost",
+  user: "root",
+  password: "Monsterofthemunich6997.",
+  database: "location-search"
+});
+
+var execute = (db, sql, args) => {
+  return new Promise((resolve, reject) => {
+    db.query(sql, args, (error, results, fields) => {
+      if (error) reject(error);
+      else resolve(results);
+    });
+  });
+};
+
+//test server/test
+router.get("/", (req, res) => {
+  res.send("NodeJS find location BackEnd by Deovani Anugrah");
+});
+
+// When user access database Person
+router.get("/location-bylonglat", (req, res) => {
+  let data = req.body;
+  let longitude = data.longitude;
+  let latitude = data.latitude;
+
+  let sql = `SELECT desa, kecamatan, kabupaten, provinsi, region, area, branch, subbranch, cluster
+            FROM locationsearch WHERE longitude = ? AND latitude = ?`;
+
+  execute(db, sql, [longitude, latitude])
+    .then(response => {
+      res.json(response[0]);
+    })
+    .catch(reason => {
+      res.status(500).json(reason);
+    });
+});
+
+module.exports = router;
